d37e31d15db6a40f7ce334fa8ac7255b7ea49dd0
jleodany <joseleodany@gmail.com>
2020-01-23 10:46:53 -0500

    Selenium and splash spiders done.

---
+++ scrapy_tutorial/spiders/SELENIUM.md
@@ -0,0 +1,13 @@
+# Using Selenium Chromedriver
+
+## Google Chrome 
+
+### First step is to have Google Chrome installed, if you don't have it yet, here https://support.google.com/chrome/answer/95346?co=GENIE.Platform%3DDesktop&hl=en-GB is the official download page.
+
+## Chromedriver
+
+### Now, it is necessary to download chromedriver, here https://chromedriver.chromium.org/ you can search for the version that matches with your Google Chrome's version.
+
+## Selenium package
+
+### And finally, it is necessary to install selenium python package, you can do it by running `pip install selenium`
--- scrapy_tutorial/spiders/splash.py
+++ scrapy_tutorial/spiders/splash.py
@@ -5,9 +5,17 @@ from scrapy_splash import SplashRequest
 
 class SplashSpider(scrapy.Spider):
     name = 'splash'
-    allowed_domains = ['stackoverflow.com']
-    start_urls = ['https://stackoverflow.com/questions?tab=Votes/']
+    allowed_domains = ['toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/js/page/2/']
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url=url, callback=self.parse, endpoint='render.html')
 
     def parse(self, response):
-        
-        pass
+        for q in response.css("div.quote"):
+            author = q.css(".author::text").extract_first()
+            quote = q.css(".text::text").extract_first()
+            # CHALLENGE get the tags for each quote using xpath.
+            yield quote
+        # CHALLENGE get the quotes from the first 4 pages.
